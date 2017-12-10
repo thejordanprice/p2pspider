1d408fcde67bf0c3161b569c2fa4fbe351697379
thejordanprice <thejordanprice@gmx.com>
2017-12-10 04:20:11 -0500

    patched mongoose open() error

--- bin/daemon.js
+++ bin/daemon.js
@@ -5,7 +5,7 @@
  */
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://127.0.0.1/magnetdb';
-mongoose.connect(mongoDB);
+mongoose.connection.openUri(mongoDB);
 
 var db = mongoose.connection;
 
